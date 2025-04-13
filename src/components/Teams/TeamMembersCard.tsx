543438fbbf6c7d663ccb3b089935fe8cfae344ed
SayanSutradhar1 <sayan321x@gmail.com>
2025-04-14 00:16:10 +0530

    team member card content modified

--- src/components/Teams/TeamMembersCard.tsx
+++ src/components/Teams/TeamMembersCard.tsx
@@ -1,8 +1,10 @@
 import { CardSpotlight } from "@/components/ui/card-spotlight";
 import { TeamMember } from "@/interfaces/team.interface";
 import Image from "next/image";
+import Link from "next/link";
+import { FaFacebook, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 
-export function TeamMemebrsCard({member}:{member:TeamMember}) {
+export function TeamMemebrsCard({ member }: { member: TeamMember }) {
   return (
     <CardSpotlight className="max-w-96 rounded-2xl px-4 py-2">
       <h1 className="text-xl text-yellow-200 text-wrap font-bold relative z-20 mt-2">
@@ -12,11 +14,30 @@ export function TeamMemebrsCard({member}:{member:TeamMember}) {
         {member.year + " " + member.department}
       </h1>
       <div className="text-neutral-200 mt-2 relative z-20">
-        <Image src={member.avatar} alt="image" className="w-[300px] p-4 h-[250px] object-cover rounded-2xl"/>
+        <Image
+          src={member.avatar}
+          alt="image"
+          className="w-[300px] p-4 h-[250px] object-cover rounded-2xl"
+        />
       </div>
       <p className="text-neutral-300 text-xl mt-2 relative z-20 capitalize">
         {member.lead ? member.lead : member.category + " Team"}
       </p>
+      <div className="flex justify-center gap-5 my-3 relative z-20">
+        {member.socials?.map((s,i) => {
+          let icon = null;
+          if (s.handler === "Facebook") {
+            icon = <FaFacebook size={35} color="white"/>;
+          } else if (s.handler === "Instagram") {
+            icon = <FaInstagram size={35} color="white"/>;
+          } else if (s.handler === "Github") {
+            icon = <FaGithub size={35} color="white"/>;
+          } else {
+            icon = <FaLinkedin size={35} color="white"/>;
+          }
+          return <Link target="_blank" key={i} href={s.link}>{icon}</Link>;
+        })}
+      </div>
     </CardSpotlight>
   );
 }
