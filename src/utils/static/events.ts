fcc1b671056fee328fa77c3ee90f5378e970e63f
SayanSutradhar1 <sayan321x@gmail.com>
2025-04-14 11:05:25 +0530

    technical events poster added

--- src/utils/static/events.ts
+++ src/utils/static/events.ts
@@ -1,30 +1,54 @@
+import e101 from "@/assets/events/TECH EXHIBITION.png"
+import e102 from "@/assets/events/GRAPHIC DESIGN.png"
+import e103 from "@/assets/events/TECHNO COMMERCIAL.png"
+import e104 from "@/assets/events/FREE FIRE.png"
+import e105 from "@/assets/events/PES.png"
+import e106 from "@/assets/events/AUTOCAD 2D CE.png"
+import e107 from "@/assets/events/BRIDGE MAKING.png"
+import e108 from "@/assets/events/CGEC MEME MADNESS.png"
+import e109 from "@/assets/events/PES.png"
+import e110 from "@/assets/events/CODE A THON.png"
+import e111 from "@/assets/events/HACK O VATION.png"
+import e112 from "@/assets/events/AUTOCAD 2D ME.png"
+import e113 from "@/assets/events/APTI QUIZ.png"
+import e114 from "@/assets/events/AUTOCAD 3D CE.png"
+import e115 from "@/assets/events/CUBE SOLVING.png"
+import e116 from "@/assets/events/PES.png"
+import e117 from "@/assets/events/ROBO SOCCER.png"
+import e118 from "@/assets/events/CHESS.png"
+import e119 from "@/assets/events/MATH.png"
+import e120 from "@/assets/events/CHEMISTRY.png"
+import e121 from "@/assets/events/PHYSICS OLYMPIAD.png"
+import e122 from "@/assets/events/MODEL MAKING.png"
+import e123 from "@/assets/events/SOLID WORKS.png"
+import e124 from "@/assets/events/PROMPT.png"
+import e125 from "@/assets/events/PES.png"
+
+
 export const staticEventsData = [
-  { uniqueId: "101", poster: "", redirect: "" },
-  { uniqueId: "102", poster: "", redirect: "" },
-  { uniqueId: "103", poster: "", redirect: "" },
-  { uniqueId: "104", poster: "", redirect: "" },
-  { uniqueId: "105", poster: "", redirect: "" },
-  { uniqueId: "106", poster: "", redirect: "" },
-  { uniqueId: "107", poster: "", redirect: "" },
-  { uniqueId: "108", poster: "", redirect: "" },
-  { uniqueId: "109", poster: "", redirect: "" },
-  { uniqueId: "110", poster: "", redirect: "" },
-  { uniqueId: "111", poster: "", redirect: "" },
-  { uniqueId: "112", poster: "", redirect: "" },
-  { uniqueId: "113", poster: "", redirect: "" },
-  { uniqueId: "114", poster: "", redirect: "" },
-  { uniqueId: "115", poster: "", redirect: "" },
-  { uniqueId: "116", poster: "", redirect: "" },
-  { uniqueId: "117", poster: "", redirect: "" },
-  { uniqueId: "118", poster: "", redirect: "" },
-  { uniqueId: "119", poster: "", redirect: "" },
-  { uniqueId: "120", poster: "", redirect: "" },
-  { uniqueId: "121", poster: "", redirect: "" },
-  { uniqueId: "122", poster: "", redirect: "" },
-  { uniqueId: "123", poster: "", redirect: "" },
-  { uniqueId: "124", poster: "", redirect: "" },
-  { uniqueId: "125", poster: "", redirect: "" },
-  { uniqueId: "126", poster: "", redirect: "" },
-  { uniqueId: "127", poster: "", redirect: "" },
-  { uniqueId: "128", poster: "", redirect: "" },
+  { uniqueId: 101, poster: e101, redirect: "/events/technical/101" },
+  { uniqueId: 102, poster: e102, redirect: "/events/technical/102" },
+  { uniqueId: 103, poster: e103, redirect: "/events/technical/103" },
+  { uniqueId: 104, poster: e104, redirect: "/events/technical/104" },
+  { uniqueId: 105, poster: e105, redirect: "/events/technical/105" },
+  { uniqueId: 106, poster: e106, redirect: "/events/technical/106" },
+  { uniqueId: 107, poster: e107, redirect: "/events/technical/107" },
+  { uniqueId: 108, poster: e108, redirect: "/events/technical/108" },
+  { uniqueId: 109, poster: e109, redirect: "/events/technical/109" },
+  { uniqueId: 110, poster: e110, redirect: "/events/technical/110" },
+  { uniqueId: 111, poster: e111, redirect: "/events/technical/111" },
+  { uniqueId: 112, poster: e112, redirect: "/events/technical/112" },
+  { uniqueId: 113, poster: e113, redirect: "/events/technical/113" },
+  { uniqueId: 114, poster: e114, redirect: "/events/technical/114" },
+  { uniqueId: 115, poster: e115, redirect: "/events/technical/115" },
+  { uniqueId: 116, poster: e116, redirect: "/events/technical/116" },
+  { uniqueId: 117, poster: e117, redirect: "/events/technical/117" },
+  { uniqueId: 118, poster: e118, redirect: "/events/technical/118" },
+  { uniqueId: 119, poster: e119, redirect: "/events/technical/119" },
+  { uniqueId: 120, poster: e120, redirect: "/events/technical/120" },
+  { uniqueId: 121, poster: e121, redirect: "/events/technical/121" },
+  { uniqueId: 122, poster: e122, redirect: "/events/technical/122" },
+  { uniqueId: 123, poster: e123, redirect: "/events/technical/123" },
+  { uniqueId: 124, poster: e124, redirect: "/events/technical/124" },
+  { uniqueId: 125, poster: e125, redirect: "/events/technical/125" },
 ];
