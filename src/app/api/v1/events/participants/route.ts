cd7286cd6237195992c4a30eb24973a0189f0de8
SayanSutradhar1 <sayan321x@gmail.com>
2025-04-17 20:56:29 +0530

    api created for fetching registration and user details

---
+++ src/app/api/v1/events/participants/route.ts
@@ -0,0 +1,46 @@
+import { Events } from "@/models/events.model";
+import { User } from "@/models/user.model";
+import { connectDB } from "@/utils/db/connect";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectDB();
+    const { uniqueId } = await req.json();
+    
+    const event = await Events.findOne({ uniqueId });
+    if (!event) {
+      return NextResponse.json({ message: "No event found" }, { status: 404 });
+    }
+
+    const participants = await Promise.all(
+      event.participants.map(async (p: any) => {
+        const user = await User.findById(p);
+        if (user) {
+          return {
+            name: user.name,
+            year: user.year,
+            department: user.department,
+            rollNumber : user.rollNumber
+          };
+        }
+        return null;
+      })
+    );
+
+    // Filter out null values in case any user was not found
+    const filteredParticipants = participants.filter(Boolean);
+
+    return NextResponse.json({
+      event: event.eventName,
+      uniqueId,
+      participants: filteredParticipants
+    });
+
+  } catch (error: any) {
+    return NextResponse.json(
+      { message: error.message || "Something went wrong" },
+      { status: 500 }
+    );
+  }
+}
