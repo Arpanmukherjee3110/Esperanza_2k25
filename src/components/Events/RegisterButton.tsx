bfeab1a2d0bee36ab6ce8b2a92276b99eb09d801
SayanSutradhar1 <sayan321x@gmail.com>
2025-04-21 00:03:37 +0530

    event registration closed

--- src/components/Events/RegisterButton.tsx
+++ src/components/Events/RegisterButton.tsx
@@ -15,41 +15,49 @@ const RegisterButton = ({
   const router = useRouter();
 
   const handleRegisterForEvent = async () => {
-    if (!userEmail) {
-      Swal.fire({
-        icon: "warning",
-        title: "Oops...",
-        text: "You need to login first!",
-        confirmButtonText: "Okay",
-      }).then(() => {
-        router.push("/login");
-      });
-    }
-    const res = await eventRegister(uniqueId, userEmail);
-    if (res) {
-      if (res.error) {
-        Swal.fire({
-          icon: "error",
-          title: "Oops...",
-          text: res.error,
-          confirmButtonText: "Okay",
-        });
-      } else {
-        Swal.fire({
-          icon: "success",
-          title: "Success",
-          text: res.message,
-          confirmButtonText: "Okay",
-        }).then(()=>{
-            router.refresh()
-        })
-      }
-    }
+    Swal.fire({
+      title: "Registration is closed",
+      icon: "info",
+      text: "Please contact the event coordinators for more information",
+    });
+    // if (!userEmail) {
+    //   Swal.fire({
+    //     icon: "warning",
+    //     title: "Oops...",
+    //     text: "You need to login first!",
+    //     confirmButtonText: "Okay",
+    //   }).then(() => {
+    //     router.push("/login");
+    //   });
+    // }
+    // const res = await eventRegister(uniqueId, userEmail);
+    // if (res) {
+    //   if (res.error) {
+    //     Swal.fire({
+    //       icon: "error",
+    //       title: "Oops...",
+    //       text: res.error,
+    //       confirmButtonText: "Okay",
+    //     });
+    //   } else {
+    //     Swal.fire({
+    //       icon: "success",
+    //       title: "Success",
+    //       text: res.message,
+    //       confirmButtonText: "Okay",
+    //     }).then(()=>{
+    //         router.refresh()
+    //     })
+    //   }
+    // }
   };
 
   return (
     <form action={handleRegisterForEvent}>
-      <Button type="submit" className="self-start bg-pink-600 hover:bg-pink-800 duration-100 cursor-pointer">
+      <Button
+        type="submit"
+        className="self-start bg-pink-600 hover:bg-pink-800 duration-100 cursor-pointer"
+      >
         Register
       </Button>
     </form>
